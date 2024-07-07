5ddaed33ca23016fc9b31a2375438d87ef069820
Purna chandra <purna90857@gmail.com>
2024-07-07 22:39:21 +0530

    initial change

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.newsapp.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY="31ec6ad575824e3b8129b06b28391fa5"
+        const val BASE_URL="https://newsapi.org/"
+        const val SEARCH_NEWS_TIME_DELAY=500L
+        const val QUERY_PAGE_SIZE=20
+    }
+
+}
